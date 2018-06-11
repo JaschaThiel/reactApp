4953354e1066a72745e0d1b1dc9325b0789fee51
Jascha Thiel <jascha.thiel@gmail.com>
2018-06-11 17:03:40 +0200

    refactoring again

--- my-app/src/game.js
+++ my-app/src/game.js
@@ -2,6 +2,34 @@ import React from 'react';
 import './index.css';
 import Board from './board';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return false;
+}
+
+function draw(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 class Game extends React.Component {
   constructor(props) {
@@ -78,35 +106,6 @@ class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return false;
-}
-
-function draw(squares) {
-  for (let i = 0; i < squares.length; i++) {
-    if (!squares[i]) {
-      return false;
-    }
-  }
-  return true;
 }
 
 export default Game;
