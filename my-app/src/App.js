53d84cad13b8fb74c058128a607f0ed30b4ceca9
JaschaThiel <jascha.thiel@gmail.com>
2018-06-15 18:49:10 +0200

    added demo appbar and drawer to be modified

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import Game from './game';
 import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
       <div><Header />
-        <div className="Game">
-          <Game />
-        </div>
       </div>
     );
   }
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import SidebarDrawer from './SidebarDrawer';
 import './Header.css';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-            Hej
+        <SidebarDrawer />
       </div>
     );
   }
